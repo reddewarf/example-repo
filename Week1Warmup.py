dba6ee5da725a61ccd259ca9069a86ece5e47d9b
Ben Caldwell <bjcaldwell@gmail.com>
2022-07-23 11:40:50 +1000

    Object-Oriented Programming added

--- Week1Warmup.py
+++ Week1Warmup.py
@@ -7,3 +7,25 @@ for i in range(0,sqrd+1):
         print(Name)
     else:
         print (i)
+
+class Engineer:
+    def __init__(self,name,type,years):
+        self.name = name
+        self.type = type
+        self.years = years
+    def getname(self):
+        print("Enginers Name: " + self.name)
+    def gettype(self):
+        print("Engineering Field: " + self.type)
+    def getexperience(self):
+        print("Years of Experience: " + self.years)
+
+Eng1 = Engineer("Bob","Civil","11")
+Eng2 = Engineer("Kev","Elec","12")
+
+Eng1.getname()
+Eng1.gettype()
+Eng1.getexperience()
+Eng2.getname()
+Eng2.gettype()
+Eng2.getexperience()
